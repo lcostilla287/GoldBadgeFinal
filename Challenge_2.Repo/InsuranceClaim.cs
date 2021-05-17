2531a7b8032464a02c01b10c3cb863b105a8d465
Luis Costilla-Guzman <lcostilla287@gmail.com>
2021-05-17 13:39:41 -0400

    Challenge 2 POCO added

---
+++ Challenge_2.Repo/InsuranceClaim.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_2.Repo
+{
+    public enum ClaimType { Car, Home, Theft}
+    class InsuranceClaim
+    {
+        public int ClaimID { get; set; }
+        public ClaimType ClaimType { get; set; }
+        public string Description { get; set; }
+        public double ClaimAmount { get; set; }
+        public DateTime DateOfIncident { get; set; }
+        public DateTime DateOfClaim { get; set; }
+        public bool IsValid 
+        { 
+            get 
+            {
+                int span = DateOfClaim.Day - DateOfIncident.Day;
+                if (span <= 30)
+                {
+                    return true;
+                }
+                else return false;
+            } 
+        }
+
+        public InsuranceClaim() { }
+
+        public InsuranceClaim(int claimID, ClaimType claimType, string description, double claimAmount, DateTime dateOfIncident, DateTime dateOfClaim)
+        {
+            ClaimID = claimID;
+            ClaimType = claimType;
+            Description = description;
+            ClaimAmount = claimAmount;
+            DateOfIncident = dateOfIncident;
+            DateOfClaim = dateOfClaim;
+        }
+    }
+}
---
+++ Challenge_2.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Challenge_2.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Challenge_2.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("70aa383d-a1ea-4785-9fea-ef6b5ef90daa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
