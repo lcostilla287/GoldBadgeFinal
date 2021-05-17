401aae81ad294ab255a7c2206bb70b74c184595f
Luis Costilla-Guzman <lcostilla287@gmail.com>
2021-05-17 12:24:03 -0400

    challenge 1 Unit Tests added

--- Challenge_1.Repo/UnitTest1.cs
+++ Challenge_1.Repo/UnitTest1.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Challenge_1.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 
 namespace Challenge_1.Repo
 {
@@ -7,8 +9,63 @@ namespace Challenge_1.Repo
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AddToDirectory_ShouldReturnTrue()
         {
+            MenuItem item = new MenuItem();
+            MenuRepo repository = new MenuRepo();
+
+            bool addResult = repository.AddMenuItemToDirectory(item);
+            Assert.IsTrue(addResult);
         }
+
+
+        //fields here
+        private MenuItem _item;
+        private MenuRepo _repo;
+
+        //Arrange
+        [TestInitialize]
+        public void Arrange()
+        {
+            _repo = new MenuRepo();
+            _item = new MenuItem(2, "CheeBurg", "Yummy", "Cheese, Hamburger, Buns", 2.00);
+            _repo.AddMenuItemToDirectory(_item);
+        }
+
+        [TestMethod]
+        public void GetByItemNumber_ShouldReturnCorrectMenuItem()
+        {
+            //Act
+            MenuItem searchResult = _repo.GetMenuItemByNumber(2);
+            //Assert
+            Assert.AreEqual(_item, searchResult);
+        }
+
+        [TestMethod]
+        public void GetDirectory_ShouldReturnTrue()
+        {
+            List<MenuItem> directory = _repo.GetMenuItems();
+
+            bool directoryHasMenuItems = directory.Contains(_item);
+            Assert.IsTrue(directoryHasMenuItems);
+        }
+
+        [TestMethod]
+        public void UpdateExistingMenuItem_ShouldReturnTrue()
+        {
+            _repo.UpdateExistingMenuItem(2, new MenuItem(3, "Hamburg", "Delicious", "Hamburger, Buns", 1.95));
+
+            Assert.AreEqual(_item.MealName, "Hamburg");
+        }
+
+        [TestMethod]
+        public void DeleteExistingMenuItem_ShouldReturnTrue()
+        {
+            bool wasDeleted = _repo.DeleteExistingMenuItem(2);
+
+            Assert.IsTrue(wasDeleted);
+        }
+
+
     }
 }
