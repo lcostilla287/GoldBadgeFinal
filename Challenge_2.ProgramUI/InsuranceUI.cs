e3ca9d1d16732452f914e1fd834e5a4a3edac58a
Luis Costilla-Guzman <lcostilla287@gmail.com>
2021-05-17 15:49:51 -0400

    Challenge 2 TakeCareOfNextClaim method added to InsuranceUI

--- Challenge_2.ProgramUI/InsuranceUI.cs
+++ Challenge_2.ProgramUI/InsuranceUI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Challenge_2.Repo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace Challenge_2.ProgramUI
 {
     public class InsuranceUI
     {
+        private InsuranceClaimRepo _repo = new InsuranceClaimRepo();
         public void Run()
         {
             Menu();
@@ -52,8 +54,68 @@ namespace Challenge_2.ProgramUI
 
         private void SeeAllClaims()
         {
+            Console.Clear();
             Console.WriteLine("Here are all of the current claims in the queue:");
+            Console.WriteLine();
+            Console.WriteLine(String.Format("{0,-10} {1,-9} {2,-15} {3,-10} {4,-15} {5,-14} {6,-10}", "ClaimID", "Type", "Description", "Amount", "DateOfAccident", "DateOfClaim", "IsValid" ));
 
+            Queue<InsuranceClaim> claimQueue = _repo.GetInsuranceClaims();
+            foreach(InsuranceClaim claims in claimQueue)
+            {
+                Console.WriteLine(String.Format("{0,-10} {1,-9} {2,-15} {3,-10} {4,-15} {5,-14} {6,-10}", claims.ClaimID, claims.ClaimType, claims.Description, claims.ClaimAmount, claims.DateOfIncident, claims.DateOfClaim, claims.IsValid));
+            }
+            Console.ReadKey();
+        }
+
+        private void TakeCareOfNextClaim()
+        {
+            Console.Clear();
+            Console.WriteLine("Here is the next claim to be handled:");
+
+            Queue<InsuranceClaim> claimQueue = _repo.GetInsuranceClaims();
+            InsuranceClaim firstClaim = claimQueue.Peek();
+            Console.WriteLine($"ClaimID: {firstClaim.ClaimID}\n" +
+                $"\n" +
+                $"Type: {firstClaim.ClaimType}\n" +
+                $"\n" +
+                $"Description: {firstClaim.Description}\n" +
+                $"\n" +
+                $"Amount: {firstClaim.ClaimAmount}\n" +
+                $"\n" +
+                $"DateOfAccident: {firstClaim.DateOfIncident}\n" +
+                $"\n" +
+                $"DateOfClaim: {firstClaim.DateOfClaim}\n" +
+                $"\n" +
+                $"IsValid: {firstClaim.IsValid}\n" +
+                $" ");
+            Console.WriteLine("Do want to deal with this claim now(y/n)?");
+
+            string response = Console.ReadLine();
+
+            switch (response)
+            {
+                case "y":
+                    HelpHandleClaim();
+                    break;
+                default:
+                    Menu();
+                    break;
+            }
+            
+        }
+
+        private void EnterANewClaim()
+        {
+            Console.Clear();
+
+
+        }
+
+        private void HelpHandleClaim()
+        {
+            Console.WriteLine("Thanks for handling this claim!");
+            _repo.HandleNextClaim();
+            Console.ReadKey();
         }
     }
 }
