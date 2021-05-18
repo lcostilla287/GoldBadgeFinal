9138e92d6b4da6658fa8821a28a06714c7ca5457
Luis Costilla-Guzman <lcostilla287@gmail.com>
2021-05-18 13:57:48 -0400

    Challenge 3 POCO Added

---
+++ Challenge_2.Repository/Badges.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_3.Repository
+{
+    public class Badges
+    {
+        public int BadgeID { get; set; }
+
+        public string[] DoorNames { get; set; }
+
+        public Badges() { }
+
+        public Badges(int badgeID, string[] doorNames)
+        {
+            BadgeID = badgeID;
+            DoorNames = doorNames;
+        }
+    }
+}
--- Challenge_2.Repository/BadgesRepo.cs
+++ Challenge_2.Repository/BadgesRepo.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Challenge_3.Repository
+namespace Challenge_2.Repository
 {
-    public class Class1
+    public class BadgesRepo
     {
     }
 }
