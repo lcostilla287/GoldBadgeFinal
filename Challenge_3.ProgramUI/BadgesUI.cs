bf5428c73e57ea50761ff5616a535eb8c392d6b5
Luis Costilla-Guzman <lcostilla287@gmail.com>
2021-05-19 11:21:26 -0400

    challenge 3 switch case added in badgeUI menu method

--- Challenge_3.ProgramUI/BadgesUI.cs
+++ Challenge_3.ProgramUI/BadgesUI.cs
@@ -15,5 +15,47 @@ namespace Challenge_3.ProgramUI
         {
             Menu();
         }
+
+        private void Menu()
+        {
+            bool keepRunning = true;
+            while (keepRunning)
+            {
+                Console.Clear();
+                Console.WriteLine("Menu\n" +
+                    " ");
+                Console.WriteLine("Hello Security Admin, What would you like to do?\n" +
+                    "1. Add a badge\n" +
+                    "2. Edit a badge\n" +
+                    "3. List all badges\n" +
+                    "4. Delete a badge\n" +
+                    "5. Exit");
+
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        AddABadge();
+                        break;
+                    case "2":
+                        EditABadge();
+                        break;
+                    case "3":
+                        ListAllBadges();
+                        break;
+                    case "4":
+                        DeleteABadge();
+                        break;
+                    case "5":
+                        keepRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please choose a valid option");
+                        break;
+                }
+            }
+
+        }
     }
 }
